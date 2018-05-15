9e83b69d64c74380e0d10254cd2d2755d44be5f2
Bas Hamar de la Brethonière <bas@basje.com>
2018-05-15 21:48:06 +0200

    Add properties to root collection contract

--- src/Collection.php
+++ src/Collection.php
@@ -2,7 +2,19 @@
 
 namespace LibCon\Collections;
 
-interface Collection extends \Traversable, \Countable
+use Countable;
+use Traversable;
+
+use LibCon\Collections\Properties\{Filterable, Mappable, Reducible};
+
+/**
+ * Base contract for all collections. Does not have any inherent order unless
+ * one is determined by an inheriting interface. May or may not contain duplicates.
+ * Bags and multisets should directly inherit from this interface.
+ *
+ * @package LibCon\Collections
+ */
+interface Collection extends Traversable, Countable, Filterable, Mappable, Reducible
 {
 
     public function isEmpty() : bool;
